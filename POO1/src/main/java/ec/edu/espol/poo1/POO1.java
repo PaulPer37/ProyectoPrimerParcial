9cd0a3c5394ef2785e0d362caf8fc57d19c43921
MatiasCollaguazo <152077122+MatiasCollaguazo@users.noreply.github.com>
2023-11-26 11:15:48 -0500

    Update POO1.java

--- POO1/src/main/java/ec/edu/espol/poo1/POO1.java
+++ POO1/src/main/java/ec/edu/espol/poo1/POO1.java
@@ -1,17 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package ec.edu.espol.poo1;
+public class POO1{
+    public static void main(String[] args){
+        Juego juego = new Juego();
+        juego.agregarJugador("Salado3000");
+        juego.agregarJugador("Maquina");
+        while (!juego.getJugadores().get(0).getMano().isEmpty() && !juego.getJugadores().get(1).getMano().isEmpty()){
+            //Juega el humano
+            System.out.println("\nTurno de " + juego.getJugadores().get(0).getNombre());
+            juego.mostrarLinea();
+            juego.getJugadores().get(0).imprimirMano();
+            juego.agregarFichaLinea(juego.getJugadores().get(0).getFicha(0), juego.getJugadores().get(0));
+            juego.machinePlay();
 
-/**
- *
- * @author RUCO HOUSE
- */
-public class POO1 {
-
-    public static void main(String[] args) {
-        System.out.println("Me ");
-        
+            //Juega la maquina
+            System.out.println("\nTurno de " + juego.getJugadores().get(juego.getJugadores().size()-1).getNombre());
+            juego.mostrarLinea();
+            juego.getJugadores().get(1).imprimirMano();
+            juego.agregarFichaLinea(juego.getJugadores().get(1).getFicha(0), juego.getJugadores().get(1));
+            juego.machinePlay();
+        }//hay que considerar quien juega primero en tal caso si la maquina o el jugador xd
     }
 }
