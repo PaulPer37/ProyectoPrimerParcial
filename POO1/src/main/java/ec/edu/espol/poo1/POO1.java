3069a8a3a9c6fe17f437901c9156ab3009d1dc1f
PaulPer37 <133935955+PaulPer37@users.noreply.github.com>
2023-11-25 20:46:53 -0500

    This
    This

---
+++ POO1/src/main/java/ec/edu/espol/poo1/POO1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.poo1;
+
+/**
+ *
+ * @author RUCO HOUSE
+ */
+public class POO1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
