5afd42f5f0ada79578b01568be24aea9f6c94061
PaulPer37 <133935955+PaulPer37@users.noreply.github.com>
2023-12-02 21:42:40 -0500

    j
    k

--- POO1/src/main/java/ec/edu/espol/poo1/POO1.java
+++ POO1/src/main/java/ec/edu/espol/poo1/POO1.java
@@ -8,73 +8,61 @@ public class POO1{
     
     public static void jugar(){
         Juego juego = new Juego();
-        
         System.out.println("Escoja una opcion: ");
          Scanner sc = new Scanner(System.in);
         sc.useDelimiter("\n");
         sc.useLocale(Locale.US);
         do{
-        System.out.println("1. Agregar Jugador");
-        System.out.println("2. Agregar maquina");
-        
-        int opcion = Integer.parseInt(sc.next());
-                while(opcion < 1 || opcion > 3){
-                System.out.println("Opción no válida, ingrese una valida:");
-                opcion = Integer.parseInt(sc.next());}
-                if (opcion == 1){
-                    System.out.println("Ingrese el nombre del jugador: ");
-                    String nombre = sc.next();
-                    while(nombre.equals("maquina")){
-                        System.out.println("Nombre no valido, elija otro");
-                        nombre = sc.next();
-                    }
-                    juego.agregarJugador(nombre);
-                }else if (opcion == 2) {
-                    System.out.println("Maquina agregada");
-                    juego.agregarJugador("maquina");
-                
-                }
-    }while(juego.getJugadores().size()<2);
+            System.out.println("1. Agregar Jugador");
+            System.out.println("2. Agregar maquina");
+            int opcion = Integer.parseInt(sc.next());
+            while(opcion < 1 || opcion > 3){
+            System.out.println("Opción no válida, ingrese una valida:");
+            opcion = Integer.parseInt(sc.next());}
+            if (opcion == 1){
+                System.out.println("Ingrese el nombre del jugador: ");
+                String nombre = sc.next();
+                juego.agregarJugador(nombre);
+            }else if (opcion == 2) {
+                System.out.println("Maquina agregada");
+                juego.agregarJugador("maquina");
+            }
+        }while(juego.getJugadores().size()<2);
         System.out.println("1. Agregar Jugador");
         System.out.println("2. Agregar maquina");
         System.out.println("3. Empezar Partida");
         int opcion = Integer.parseInt(sc.next());
-            while(opcion < 1 || opcion > 3){
+        while(opcion < 1 || opcion > 3){
             System.out.println("Opción no válida, ingrese una valida:");
-            opcion = Integer.parseInt(sc.next());}
-            while(opcion == 1 || opcion == 2){
-                if (opcion == 1){
-                    System.out.println("Ingrese el nombre del jugador: ");
-                    String nombre = sc.next();
-                    while(nombre.equals("maquina")){
-                        System.out.println("Nombre no valido, elija otro");
-                        nombre = sc.next();
-                    }
-                    juego.agregarJugador(nombre);
-                    
-                    System.out.println("1. Agregar Jugador");
-                    System.out.println("2. Agregar maquina");
-                    System.out.println("3. Empezar Partida");
-                    opcion = Integer.parseInt(sc.next());
-                }else if (opcion == 2) {
-                    System.out.println("Maquina agregada");
-                    
-                    juego.agregarJugador("maquina");
-                    System.out.println("1. Agregar Jugador");
-                    System.out.println("2. Agregar maquina");
-                    System.out.println("3. Empezar Partida");
-                    opcion = Integer.parseInt(sc.next());
-                }
-                }
-        System.out.println("Que empieze el juego");
+            opcion = Integer.parseInt(sc.next());
+        }
+        while(opcion == 1 || opcion == 2){
+            if (opcion == 1){
+                System.out.println("Ingrese el nombre del jugador: ");
+                String nombre = sc.next();
+                juego.agregarJugador(nombre);
+                System.out.println("1. Agregar Jugador");
+                System.out.println("2. Agregar maquina");
+                System.out.println("3. Empezar Partida");
+                opcion = Integer.parseInt(sc.next());
+            }else if (opcion == 2) {
+                System.out.println("Maquina agregada");
+                juego.agregarJugador("maquina");
+                System.out.println("1. Agregar Jugador");
+                System.out.println("2. Agregar maquina");
+                System.out.println("3. Empezar Partida");
+                opcion = Integer.parseInt(sc.next());
+            }
+        }
+        System.out.println("Que empiece el juego");
         boolean fin = false;
         do{
-            
-
             for(Jugador jugador:juego.getJugadores()){
-                System.out.println("\nTurno de " + jugador.getNombre());
+                System.out.print("\nTurno de " + jugador.getNombre() + ": Mano -> ");
+                jugador.imprimirMano();
+                System.out.print("\nLinea de juego -> ");
                 juego.mostrarLinea();
-                System.out.println("\n");
+                System.out.println();
                 if(!POO1.existeFichaComodin(jugador.getMano())){
                     if(POO1.terminar(jugador.getMano(), juego)){
                         System.out.println("Perdio "+jugador.getNombre()+", no tiene fichas usables");
@@ -83,18 +71,37 @@ public class POO1{
                     }
                 }
                 if(!jugador.getNombre().equals("maquina")){
-                    jugador.imprimirMano();
-                    System.out.println("\nIngrese la posicion de la ficha que quiere usar");
+                    System.out.print("\nÍndice de ficha para jugar (0 es el primero): ");
                     opcion = Integer.parseInt(sc.next());
-                    System.out.println(jugador.getFicha(opcion-1));
-                    boolean a = juego.agregarFichaLinea(jugador.getFicha(opcion-1), jugador);
-                    while(!a){
-                        System.out.println("Ficha no valida, ingrese otra: ");
+                    while(opcion<0 || opcion>(jugador.getMano().size()-1)){
+                        System.out.println("Ficha no válida");
+                        System.out.print("\nLinea de juego -> ");
+                        juego.mostrarLinea();
+                        System.out.print("\nMano -> ");
+                        jugador.imprimirMano();
+                        System.out.println("\nIngrese nuevamente un indice de ficha (0 es el primero): ");
+                        opcion = Integer.parseInt(sc.next());
+                    }
+                    boolean esFichaValida = juego.agregarFichaLinea(jugador.getFicha(opcion), jugador);
+                    if (esFichaValida){
+                        System.out.println("Movimiento Valido");
+                        System.out.print("Nueva Línea de juego -> ");
+                        juego.mostrarLinea();
+                        System.out.println();
+                    }
+                    while(!esFichaValida){
+                        System.out.print("Ficha no valida, ingrese otra: ");
                         jugador.imprimirMano();
                         System.out.println("\n");
                         opcion = Integer.parseInt(sc.next());
-                        System.out.println(jugador.getFicha(opcion-1));
-                        a = juego.agregarFichaLinea(jugador.getFicha(opcion-1), jugador);
+                        System.out.println(jugador.getFicha(opcion));
+                        esFichaValida = juego.agregarFichaLinea(jugador.getFicha(opcion), jugador);
+                        if (esFichaValida){
+                            System.out.println("Movimiento Valido");
+                            System.out.print("Nueva Línea de juego -> ");
+                            juego.mostrarLinea();
+                            System.out.println();
+                        }
                     }
                     if(jugador.getMano().isEmpty()){
                         System.out.println("Gano "+jugador.getNombre());
